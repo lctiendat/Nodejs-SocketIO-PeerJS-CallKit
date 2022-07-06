d8ea64300864edcbf5edc9a8681b7086cbf23df5
lctiendat <lctiendat@gmail.com>
2022-07-06 09:38:13 +0700

    okoko

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
@@ -16,10 +17,27 @@ app.get("/room", (req, res) => {
   res.render("room");
 });
 
-io.on("connection", socket => {
-    console.log("a user connected");
-    socket.join(socket.id);
-})
+io.on("connection", async (socket) => {
+  console.log("a user connected");
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    socket.broadcast.to(room).emit("userJoined", socket.id);
+  });
+
+  socket.on("volume-action", (data) => {
+    socket.broadcast.to(data.room).emit("volume-action", data);
+  });
+  socket.on("camera-action", (data) => {
+    console.log(data);
+    socket.broadcast.to(data.room).emit("camera-action", data);
+  });
+
+  socket.on("cancel-call", (room) => {
+    socket.broadcast.to(room).emit("cancel-call", room);
+  });
+
+  socket.on("disconnect", () => {});
+});
 
 server.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
